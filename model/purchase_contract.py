19062d6536f9080ea453f8e2d4f9cae5c2bbf8bc
Valentin Thirion <valentinthirion@gmail.com>
2016-08-10 17:33:22 +0200

    Added []

--- model/purchase_contract.py
+++ model/purchase_contract.py
@@ -1,6 +1,6 @@
 from openerp import models, fields
 
 class PurchaseContractWithAttachment(models.Model):
-    _inherit = 'sale.subscription'
+    _inherit = ['sale.subscription']
     
     attachments_ids = fields.Many2many('ir.attachment', string="Attachments")
