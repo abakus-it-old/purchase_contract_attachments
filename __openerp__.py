b6d95385b45746a6757dcfc5ba6a12230adec75a
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 14:25:11 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Attachments on purchase contracts",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'author': "Valentin THIRION, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'Purchase',
